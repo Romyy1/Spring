66bc2712ba2754975290c6721e7937575bfcc909
Romyy1 <121878923+Romyy1@users.noreply.github.com>
2025-11-09 19:17:38 +0100

    Model

--- springboot-webapp/src/main/java/com/primer/webapp/springboot_webapp/Controllers/UserRestController.java
+++ springboot-webapp/src/main/java/com/primer/webapp/springboot_webapp/Controllers/UserRestController.java
@@ -3,10 +3,13 @@ package com.primer.webapp.springboot_webapp.Controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.catalina.User;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.primer.webapp.springboot_webapp.Models.UserModel;
 
 
 @RestController
@@ -16,14 +19,12 @@ public class UserRestController {
 
     @GetMapping("/details")
     public Map<String,Object> details(Model model){
-
+        UserModel user = new UserModel("Alejandro","Román Santos");
         Map<String,Object> body= new HashMap<>();
         body.put("title", "Hola mundo Spring Boot");
-        body.put("user", "Hola mundo soy Alejandro");
-        body.put("ejemplo", "Hola mundo esto es un ejemplo");
+        body.put("user", user);
 
-        
-
+    
         return body;
 
     }
---
+++ springboot-webapp/src/main/java/com/primer/webapp/springboot_webapp/Models/UserModel.java
@@ -0,0 +1,30 @@
+package com.primer.webapp.springboot_webapp.Models;
+
+public class UserModel {
+    private String name,lastname;
+
+    
+    
+    public UserModel(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    
+}
